c8528638e22efbf6763134688c66dd0c39020ad7
okaygoodluck <okaygoodluck@myyahoo.com>
2026-06-01 10:48:27 -0300

    chore: remover scripts batch obsoletos e redundantes do projeto

--- CRIAR_ZIP_HOME_OFFICE.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo [INFO] Iniciando criador de versao portatil...
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0GERAR_VERSAO_PORTATIL.ps1"
-pause
--- iniciar_verificador_regras.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set GDIS_PORT=8765
-start "Plataforma GDIS" python -m src.api.app_unificado
-timeout /t 1 /nobreak >nul
-start "" "http://127.0.0.1:%GDIS_PORT%/"
